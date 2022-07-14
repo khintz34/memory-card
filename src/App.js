3bf840394752447d3f904b5a997d2221ef2bd7e4
Kevin Hintz <khintz34@gmail.com>
2022-07-14 16:34:40 -0500

    shuffle cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import ScoreBoard from "./components/ScoreBoard";
 import Title from "./components/Title";
 import CardBox from "./components/CardBox";
 import React, { useEffect, useState } from "react";
+import {
+  faFutbol,
+  faFootball,
+  faBaseball,
+  faBasketball,
+  faHockeyPuck,
+  faTableTennis,
+  faVolleyball,
+  faGolfBall,
+  faBroomBall,
+  faBowlingBall,
+  faBicycle,
+  faDumbbell,
+} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const resetList = {
@@ -19,16 +33,35 @@ function App() {
     card11: false,
     card12: false,
   };
+
+  const [orderedArray, setOrderedArray] = useState([
+    { title: "card1", icon: faFutbol, color: "#d4f0f0" },
+    { title: "card2", icon: faFootball, color: "#d4f0f0" },
+    { title: "card3", icon: faBaseball, color: "#8fcaca" },
+    { title: "card4", icon: faGolfBall, color: "#8fcaca" },
+    { title: "card5", icon: faHockeyPuck, color: "#cce2cb" },
+    { title: "card6", icon: faBasketball, color: "#cce2cb" },
+    { title: "card7", icon: faBroomBall, color: "#ffc5bf" },
+    { title: "card8", icon: faTableTennis, color: "#ffc5bf" },
+    { title: "card9", icon: faBowlingBall, color: "#ffd8be" },
+    { title: "card10", icon: faVolleyball, color: "#ffd8be" },
+    { title: "card11", icon: faBicycle, color: "#fee1e8" },
+    { title: "card12", icon: faDumbbell, color: "#fee1e8" },
+  ]);
+
   const [currentScore, setCurrentScore] = useState(0);
+
   const [highScore, setHighScore] = useState(() => {
     return localStorage.getItem("highScore") || 0;
   });
+
   const [list, setList] = useState(resetList);
 
   const buttonClick = (title) => {
     if (list[title] === false) {
       setList({ ...list, [title]: true });
       setCurrentScore(currentScore + 1);
+      shuffleArray(orderedArray);
     } else {
       setCurrentScore(0);
       setList(resetList);
@@ -45,12 +78,16 @@ function App() {
     localStorage.setItem("highScore", highScore);
   }, [highScore]);
 
+  const shuffleArray = () => {
+    setOrderedArray([...orderedArray].sort(() => Math.random() - 0.5));
+  };
+
   return (
     <div className="App">
       <Title />
       <p>Its Simple: Don't click the same card twice!</p>
       <ScoreBoard currentScore={currentScore} highScore={highScore} />
-      <CardBox click={buttonClick} />
+      <CardBox click={buttonClick} orderedArray={orderedArray} />
     </div>
   );
 }
--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,93 +1,22 @@
 import "../styles/App.css";
 import React from "react";
 import Card from "./Card";
-import {
-  faFutbol,
-  faFootball,
-  faBaseball,
-  faBasketball,
-  faHockeyPuck,
-  faTableTennis,
-  faVolleyball,
-  faGolfBall,
-  faBroomBall,
-  faBowlingBall,
-  faBicycle,
-  faDumbbell,
-} from "@fortawesome/free-solid-svg-icons";
 
 const CardBox = (props) => {
-  return (
-    <div id="CardBox">
-      <Card title="card1" icon={faFutbol} color="#d4f0f0" click={props.click} />
+  //   console.log(props.orderedArray);
+  let itemArray = props.orderedArray.map((index, i) => {
+    return (
       <Card
-        title="card2"
-        icon={faFootball}
-        color="#d4f0f0"
+        title={index.title}
+        icon={index.icon}
+        color={index.color}
         click={props.click}
+        key={`cards-${i}`}
       />
-      <Card
-        title="card3"
-        icon={faBaseball}
-        color="#8fcaca"
-        click={props.click}
-      />
-      <Card
-        title="card4"
-        icon={faGolfBall}
-        color="#8fcaca"
-        click={props.click}
-      />
-      <Card
-        title="card5"
-        icon={faHockeyPuck}
-        color="#cce2cb"
-        click={props.click}
-      />
-      <Card
-        title="card6"
-        icon={faBasketball}
-        color="#cce2cb"
-        click={props.click}
-      />
-      <Card
-        title="card7"
-        icon={faBroomBall}
-        color="#ffc5bf"
-        click={props.click}
-      />
-      <Card
-        title="card8"
-        icon={faTableTennis}
-        color="#ffc5bf"
-        click={props.click}
-      />
-      <Card
-        title="card9"
-        icon={faBowlingBall}
-        color="#ffd8be"
-        click={props.click}
-      />
-      <Card
-        title="card10"
-        icon={faVolleyball}
-        color="#ffd8be"
-        click={props.click}
-      />
-      <Card
-        title="card11"
-        icon={faBicycle}
-        color="#fee1e8"
-        click={props.click}
-      />
-      <Card
-        title="card12"
-        icon={faDumbbell}
-        color="#fee1e8"
-        click={props.click}
-      />
-    </div>
-  );
+    );
+  });
+
+  return <div id="CardBox">{itemArray}</div>;
 };
 
 export default CardBox;
